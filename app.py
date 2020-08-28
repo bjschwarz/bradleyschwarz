37a9c21dbf95326b6c0d4c47e1e0369fff1001e7
bradley schwarz <bradleyschwarz1@gmail.com>
2020-08-28 11:44:36 -0400

    boilerplate added

--- app.py
+++ app.py
@@ -1,108 +1,24 @@
-#----------------------------------------------------------------------------#
-# Imports
-#----------------------------------------------------------------------------#
-
-from flask import Flask, render_template, request
-# from flask_sqlalchemy import SQLAlchemy
-import logging
-from logging import Formatter, FileHandler
-from forms import *
-# from models import *
+from flask import Flask, url_for, render_template, send_file, current_app
 import os
 
-#----------------------------------------------------------------------------#
-# App Config.
-#----------------------------------------------------------------------------#
-
 app = Flask(__name__)
-app.config.from_object('config')
-# db = SQLAlchemy(app)
 
-# Automatically tear down SQLAlchemy.
-
-# @app.teardown_request
-# def shutdown_session(exception=None):
-#     db_session.remove()
-
-
-# Login required decorator.
-
-# def login_required(test):
-#     @wraps(test)
-#     def wrap(*args, **kwargs):
-#         if 'logged_in' in session:
-#             return test(*args, **kwargs)
-#         else:
-#             flash('You need to login first.')
-#             return redirect(url_for('login'))
-#     return wrap
-
-#----------------------------------------------------------------------------#
-# Controllers.
-#----------------------------------------------------------------------------#
-
-
-@app.route('/')
+@app.route("/")
 def home():
-    return render_template('pages/placeholder.home.html')
+  '''
+  return the home page
+  '''
+  return render_template('home.html')
+
+@app.route("/download_resume")
+def download_resume():
+  '''
+  download resume
+  '''
+  current_path = os.path.join(current_app.static_folder, 'img')
+  path = os.path.join(current_path, 'resume.pdf')
+  return send_file(path, as_attachment=True)
 
 
-@app.route('/about')
-def about():
-    return render_template('pages/placeholder.about.html')
-
-
-@app.route('/login')
-def login():
-    form = LoginForm(request.form)
-    return render_template('forms/login.html', form=form)
-
-
-@app.route('/register')
-def register():
-    form = RegisterForm(request.form)
-    return render_template('forms/register.html', form=form)
-
-
-@app.route('/forgot')
-def forgot():
-    form = ForgotForm(request.form)
-    return render_template('forms/forgot.html', form=form)
-
-# Error handlers.
-
-
-@app.errorhandler(500)
-def internal_error(error):
-    # db_session.rollback()
-    return render_template('errors/500.html'), 500
-
-
-@app.errorhandler(404)
-def not_found_error(error):
-    return render_template('errors/404.html'), 404
-
-if not app.debug:
-    file_handler = FileHandler('error.log')
-    file_handler.setFormatter(
-        Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]')
-    )
-    app.logger.setLevel(logging.INFO)
-    file_handler.setLevel(logging.INFO)
-    app.logger.addHandler(file_handler)
-    app.logger.info('errors')
-
-#----------------------------------------------------------------------------#
-# Launch.
-#----------------------------------------------------------------------------#
-
-# Default port:
 if __name__ == '__main__':
-    app.run()
-
-# Or specify port manually:
-'''
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
-'''
+  app.run(port=5000)
