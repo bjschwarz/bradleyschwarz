e844268acac8e510cd7a6d0915b3fed220909c39
bradley schwarz <bradleyschwarz1@gmail.com>
2024-09-20 09:04:56 -0400

    added more work

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -16,7 +16,7 @@ import dryingrack1 from "../../public/images/20240509_springrelease_186.jpg"
 import dryingrack2 from "../../public/images/20240509_springrelease_166.jpg"
 import dryingrack3 from "../../public/images/20240428_dryingracks_088.jpg"
 import dryingrack4 from "../../public/images/20240509_springrelease_194.jpg"
-import dryingrack5 from "../../public/images/20240428_dryingracks_132.jpg"
+import dryingrack5 from "../../public/images/20240428_dryingracks_029.jpg"
 
 
 import candelabra1 from "../../public/images/20240513_candelabra_023.jpg"
@@ -42,8 +42,93 @@ import fireplacetoolset3 from "../../public/images/20230302_fireplacetools1901.j
 import fireplacetoolset4 from "../../public/images/20230302_fireplacetools1905.jpg"
 import fireplacetoolset5 from "../../public/images/20230302_fireplacetools1912.jpg"
 
+import spatula1 from "../../public/images/20240915_portfolio_009.jpg"
+import spatula2 from "../../public/images/20240915_portfolio_010.jpg"
+import spatula3 from "../../public/images/20240915_portfolio_018.jpg"
+import spatula4 from "../../public/images/20240915_portfolio_019.jpg"
+
+import crablock1 from "../../public/images/20240915_portfolio_037.jpg"
+import crablock2 from "../../public/images/20240915_portfolio_042.jpg"
+import crablock3 from "../../public/images/20240915_portfolio_048.jpg"
+import crablock4 from "../../public/images/20240915_portfolio_049.jpg"
+import crablock5 from "../../public/images/20240915_portfolio_050.jpg"
+import crablock6 from "../../public/images/20240915_portfolio_054.jpg"
+
+import brassgreenman1 from "../../public/images/20240915_portfolio_071.jpg"
+import brassgreenman2 from "../../public/images/20240915_portfolio_087.jpg"
+import brassgreenman3 from "../../public/images/20240915_portfolio_070.jpg"
+import brassgreenman4 from "../../public/images/20240915_portfolio_125.jpg"
+
+import brassface1 from "../../public/images/20240915_portfolio_095.jpg"
+import brassface2 from "../../public/images/20240915_portfolio_080.jpg"
+import brassface3 from "../../public/images/20240915_portfolio_078.jpg"
+import brassface4 from "../../public/images/20240915_portfolio_096.jpg"
+
+import irongreenman1 from "../../public/images/20240915_portfolio_113.jpg"
+import irongreenman2 from "../../public/images/20240915_portfolio_100.jpg"
+import irongreenman4 from "../../public/images/20240915_portfolio_134.jpg"
+import irongreenman5 from "../../public/images/20240915_portfolio_108.jpg"
+
+import shovel1 from "../../public/images/20240915_portfolio_146.jpg"
+import shovel2 from "../../public/images/20240915_portfolio_153.jpg"
+import shovel3 from "../../public/images/20240915_portfolio_154.jpg"
+
+import twocandlechandelier1 from "../../public/images/20240915_portfolio_167.jpg"
+import twocandlechandelier2 from "../../public/images/20240915_portfolio_173.jpg"
+import twocandlechandelier3 from "../../public/images/20240915_portfolio_177.jpg"
+import twocandlechandelier4 from "../../public/images/20240915_portfolio_179.jpg"
+import twocandlechandelier5 from "../../public/images/20240915_portfolio_182.jpg"
+
 const projects = [
 
+    {
+        img: candelabra1,
+        medium: 'Mild Steel',
+        title: 'Candelabra',
+        additional_images: [
+            {   
+                title: 'Candelabra 1',
+                img: candelabra1
+            },
+            {   
+                title: 'Candelabra 2',
+                img: candelabra2
+            },
+            {   
+                title: 'Candelabra 3',
+                img: candelabra3
+            },
+            {   
+                title: 'Candelabra 4',
+                img: candelabra4
+            },
+        ],
+        description: "This project was based on an image of a 14th century candelabra found in the book Decorative Antique Ironwork by Henry Rene D'Allemagne.. I was drawn immediately to the integrated dragon head legs. This detail provided an opportunity to try solid steel chasing. The project was a combination of sculptural and geometrical design. I am interested in exploring the design of how candle light was optimized and made beautiful in times past. They were the main source of lighting at tables and in large rooms for hundreds of years."
+    },
+    {
+        img: brassface1,
+        medium: 'brass',
+        title: 'Brass Face',
+        additional_images: [
+            {   
+                title: 'Brass Face 1',
+                img: brassface1
+            },
+            {   
+                title: 'Brass Face 2',
+                img: brassface2
+            },
+            {   
+                title: 'Brass Face 3',
+                img: brassface3
+            },
+            {   
+                title: 'Brass Face 4',
+                img: brassface4
+            },
+        ],
+        description: "This repousse project was an anatomical study of the human face in brass. I wanted to learn how to render more realistic faces so I referenced some anatomical sculptures for this piece. The plate is designed to be mounted to a door or chest."
+    },
     {
         img: leafhook1,
         medium: 'Mild Steel',
@@ -104,30 +189,7 @@ const projects = [
         ],
         description: "These hooks were a commission for a client. They wanted an oak-themed hooks for the entryway of their home that is shaded by a magnificent white oak tree. I found inspiration for the project in gothic-style leaves on the andirons at the Biltmore Estate. I appreciated their simplicity of the central stems. The simple stems made for an elegant leaf free of distraction. The stem then curves to make the base of the hook and terminates in an acorn. "
     },
-    {
-        img: candelabra1,
-        medium: 'Mild Steel',
-        title: 'Candelabra',
-        additional_images: [
-            {   
-                title: 'Candelabra 1',
-                img: candelabra1
-            },
-            {   
-                title: 'Candelabra 2',
-                img: candelabra2
-            },
-            {   
-                title: 'Candelabra 3',
-                img: candelabra3
-            },
-            {   
-                title: 'Candelabra 4',
-                img: candelabra4
-            },
-        ],
-        description: "This project was based on an image of a 14th century candelabra found in the book Decorative Antique Ironwork by Henry Rene D'Allemagne.. I was drawn immediately to the integrated dragon head legs. This detail provided an opportunity to try solid steel chasing. The project was a combination of sculptural and geometrical design. I am interested in exploring the design of how candle light was optimized and made beautiful in times past. They were the main source of lighting at tables and in large rooms for hundreds of years."
-    },
+    
     {
         img: dividers1,
         medium: 'Mild Steel',
@@ -235,6 +297,164 @@ const projects = [
             },
         ],
         description: "While renovating our home, we went several winters heating with only a fireplace insert. This was a great opportunity to create a nice fireplace toolset for regular use. I wanted to make wooden handles for the tools and fix them to the steel with a through tang similar to how draw knives are made. At the time this was a challenging project that pushed me in many areas."
+    },
+
+    {
+        img: spatula1,
+        medium: 'Mild Steel',
+        title: 'Spatula',
+        additional_images: [
+            {   
+                title: 'Spatula 1',
+                img: spatula1
+            },
+            {   
+                title: 'Spatula 2',
+                img: spatula2
+            },
+            {   
+                title: 'Spatula 3',
+                img: spatula3
+            },
+            {   
+                title: 'Spatula 4',
+                img: spatula4
+            },
+         
+        ],
+        description: "The inspiration for this ornate keyhole spatula came from a picture of Tom Latané's utensils. I wanted to do a project that utilized a swaged molding detail. The positive shape was first made on the metal lathe and then transferred into a top and bottom die to swage onto the spatula."
+    },
+    {
+        img: crablock1,
+        medium: 'Mild Steel',
+        title: 'Crab Lock',
+        additional_images: [
+            {   
+                title: 'Crab Lock 1',
+                img:crablock1
+            },
+            {   
+                title: 'Crab Lock 2',
+                img:crablock2
+            },
+            {   
+                title: 'Crab Lock 3',
+                img:crablock3
+            },
+            {   
+                title: 'Crab Lock 4',
+                img:crablock4
+            },
+            {   
+                title: 'Crab Lock 5',
+                img:crablock5
+            },
+            {   
+                title: 'Crab Lock 6',
+                img:crablock6
+            },
+        ],
+        description: "Peter Ross had a couple orders for crab locks and offered for me to come learn to make one while he made his. It was a challenging but interesting project. I had not done many small part forgings before. The lock operates well, but I would change some things to improve the performance with the next one."
+    },
+    {
+        img: brassgreenman1,
+        medium: 'brass',
+        title: 'Brass Green Man',
+        additional_images: [
+            {   
+                title: 'Brass Green Man 1',
+                img: brassgreenman1
+            },
+            {   
+                title: 'Brass Green Man 2',
+                img: brassgreenman2
+            },
+            {   
+                title: 'Brass Green Man 3',
+                img: brassgreenman3
+            },
+            {   
+                title: 'Brass Green Man 4',
+                img: brassgreenman4
+            },
+        ],
+        description: "This was my first attempt with chasing and repousse. Unfortunately I punched through the brass in a couple places since I had not learned the limitations of the medium or the importance of having tools with all smooth edges. Nevertheless I really enjoyed this project and it inspired me to continue doing repousse work."
+    },
+    {
+        img: irongreenman1,
+        medium: 'Mild Steel',
+        title: 'Iron Green Man',
+        additional_images: [
+            {   
+                title: 'Iron Green Man 1',
+                img: irongreenman1
+            },
+            {   
+                title: 'Iron Green Man 2',
+                img: irongreenman2
+            },
+            {   
+                title: 'Iron Green Man 4',
+                img: irongreenman4
+            },
+            {   
+                title: 'Iron Green Man 5',
+                img: irongreenman5
+            },
+        ],
+        description: "This was my first attempt at chasing and repousse in steel. Moving cold steel by hand and hammer is much different than non-ferrous metals. I learned that I needed a bigger hammer with more force and frequent annealing to keep the metal soft enough to work. It gave me an appreciation for medieval armor smiths. I originally designed this to become a door knocker and I eventually will complete it."
+    },
+
+    
+
+    {
+        img: shovel1,
+        medium: 'mild steel',
+        title: 'Heart Shovel',
+        additional_images: [
+            {   
+                title: 'Heart Shovel 1',
+                img: shovel1
+            },
+            {   
+                title: 'Heart Shovel 2',
+                img: shovel2
+            },
+            {   
+                title: 'Heart Shovel 3',
+                img: shovel3
+            }
+        ],
+        description: "I was inspired by some colonial French designs that incorporated a welded heart and wanted to try to make one on a shovel. At the time this was a difficult exercise for me and pushed my forge welding skills to the next level."
+    },
+
+    {
+        img: twocandlechandelier1,
+        medium: 'mild steel',
+        title: 'Two Candle Chandelier',
+        additional_images: [
+            {   
+                title: 'Two Candle Chandelier 1',
+                img: twocandlechandelier1
+            },
+            {   
+                title: 'Two Candle Chandelier 2',
+                img: twocandlechandelier2
+            },
+            {   
+                title: 'Two Candle Chandelier 3',
+                img: twocandlechandelier3
+            },
+            {   
+                title: 'Two Candle Chandelier 4',
+                img: twocandlechandelier4
+            },
+            {   
+                title: 'Two Candle Chandelier 5',
+                img: twocandlechandelier5
+            },
+        ],
+        description: "Tom Latané made a very similar two candle chandelier. I thought the design was fantastic and featured numerous types of forging that I had never done before. The scrolls were especially hard for me at the time. I learned the importance of having quality scrolling tongs that don't bend when twisting."
     },
 ]
 
