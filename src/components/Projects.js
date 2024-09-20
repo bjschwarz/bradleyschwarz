460962334385c6d21f4a738f3e170607feef7a9a
bradley schwarz <bradleyschwarz1@gmail.com>
2024-09-20 11:49:12 -0400

    shuffle

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -12,8 +12,8 @@ import flowerhook2 from "../../public/images/20240509_springrelease_003.jpg"
 import wallsconce1 from "../../public/images/20240509_springrelease_040.jpg"
 import wallsconce2 from "../../public/images/20240509_springrelease_073.jpg"
 
-import dryingrack1 from "../../public/images/20240509_springrelease_186.jpg"
-import dryingrack2 from "../../public/images/20240509_springrelease_166.jpg"
+import dryingrack1 from "../../public/images/20240509_springrelease_166.jpg"
+import dryingrack2 from "../../public/images/20240509_springrelease_186.jpg"
 import dryingrack3 from "../../public/images/20240428_dryingracks_088.jpg"
 import dryingrack4 from "../../public/images/20240509_springrelease_194.jpg"
 import dryingrack5 from "../../public/images/20240428_dryingracks_029.jpg"
@@ -129,22 +129,7 @@ const projects = [
         ],
         description: "This repousse project was an anatomical study of the human face in brass. I wanted to learn how to render more realistic faces so I referenced some anatomical sculptures for this piece. The plate is designed to be mounted to a door or chest."
     },
-    {
-        img: leafhook1,
-        medium: 'Mild Steel',
-        title: 'Leaf Hook',
-        additional_images: [
-            {   
-                title: 'Leaf Hook 1',
-                img: leafhook1
-            },
-            {   
-                title: 'Leaf Hook 2',
-                img: leafhook2
-            },
-        ],
-        description: "This hook was inspired by early medieval Gothic-era ironwork, specifically the hinges of the Notre Dame Cathedral. I wanted to develop the process of creating these botanical motifs, using some of the same ancient techniques, to use on more complex future work. There is an elegance and mastery to these shapes that I am only scratching the surface of."
-    },
+    
     {
         img: trivet1,
         medium: 'Mild Steel',
@@ -189,27 +174,6 @@ const projects = [
         ],
         description: "These hooks were a commission for a client. They wanted an oak-themed hooks for the entryway of their home that is shaded by a magnificent white oak tree. I found inspiration for the project in gothic-style leaves on the andirons at the Biltmore Estate. I appreciated their simplicity of the central stems. The simple stems made for an elegant leaf free of distraction. The stem then curves to make the base of the hook and terminates in an acorn. "
     },
-    
-    {
-        img: dividers1,
-        medium: 'Mild Steel',
-        title: 'Gothic Compass',
-        additional_images: [
-            {   
-                title: 'Dividers 1',
-                img: dividers1
-            },
-            {   
-                title: 'Dividers 2',
-                img: dividers2
-            },
-            {   
-                title: 'Dividers 3',
-                img: dividers3
-            },
-        ],
-        description: "I was inspired by mentor and fellow smith Tom Latané who made a gothic compass years ago. Additionally, I referenced an image of a similar compass in the book Decorative Antique Ironwork by Henry Rene D'Allemagne. This was a challenging project since I first had to learn the proper process of forging a mechanically-sound compass. Tools have always been an extension of the hand for craftsmen. I enjoy making tools beautiful in addition to functional. Tools made with this intention are a joy to use."
-    },
     {
         img: flowerhook1,
         medium: 'Mild Steel',
@@ -226,22 +190,8 @@ const projects = [
         ],
         description: "This hook was inspired by early medieval Gothic-era ironwork, specifically the hinges of the Notre Dame Cathedral. I wanted to develop the process of creating these botanical motifs, using some of the same ancient techniques, to use on more complex future work. There is an elegance and mastery to these shapes that I am only scratching the surface of."
     },
-    {
-        img: wallsconce1,
-        medium: 'Mild Steel',
-        title: 'Wall Sconce',
-        additional_images: [
-            {   
-                title: 'Wall Sconce 1',
-                img: wallsconce1
-            },
-            {   
-                title: 'Wall Sconce 2',
-                img: wallsconce2
-            },
-        ],
-        description: "We are fond of using candles for light in our household. Taking the candles off surfaces where children and animals can reach them makes it much safer. Since wall mounted candles have been used for centuries I don't think we are the first to have seen the benefit. The inspiration for this design came from a similar one made by mentor and fellow smith Tom Latané. I loved the extreme material isolation and the integrated candle cup. "
-    },
+    
+    
     {
         img: dryingrack1,
         medium: 'Mild Steel, Red Oak',
@@ -270,6 +220,27 @@ const projects = [
         ],
         description: "Most of the accordion-style drying racks available online are poor quality and cannot hold a high quantity of clothes. We faced this problem when we were looking to replace one that fell apart and couldn't be repaired. I found some examples of antique wall-mount drying racks that had an ingenious design. I decided it was an excellent opportunity to bring back the old mentality of beautiful form and function. I used proportional geometry to do all of the layout for this piece. "
     },
+    {
+        img: dividers1,
+        medium: 'Mild Steel',
+        title: 'Gothic Compass',
+        additional_images: [
+            {   
+                title: 'Dividers 1',
+                img: dividers1
+            },
+            {   
+                title: 'Dividers 2',
+                img: dividers2
+            },
+            {   
+                title: 'Dividers 3',
+                img: dividers3
+            },
+        ],
+        description: "I was inspired by mentor and fellow smith Tom Latané who made a gothic compass years ago. Additionally, I referenced an image of a similar compass in the book Decorative Antique Ironwork by Henry Rene D'Allemagne. This was a challenging project since I first had to learn the proper process of forging a mechanically-sound compass. Tools have always been an extension of the hand for craftsmen. I enjoy making tools beautiful in addition to functional. Tools made with this intention are a joy to use."
+    },
+    
     {
         img: fireplacetoolset1,
         medium: 'Mild Steel, Ashe',
@@ -381,54 +352,6 @@ const projects = [
         description: "This was my first attempt with chasing and repousse. Unfortunately I punched through the brass in a couple places since I had not learned the limitations of the medium or the importance of having tools with all smooth edges. Nevertheless I really enjoyed this project and it inspired me to continue doing repousse work."
     },
     {
-        img: irongreenman1,
-        medium: 'Mild Steel',
-        title: 'Iron Green Man',
-        additional_images: [
-            {   
-                title: 'Iron Green Man 1',
-                img: irongreenman1
-            },
-            {   
-                title: 'Iron Green Man 2',
-                img: irongreenman2
-            },
-            {   
-                title: 'Iron Green Man 4',
-                img: irongreenman4
-            },
-            {   
-                title: 'Iron Green Man 5',
-                img: irongreenman5
-            },
-        ],
-        description: "This was my first attempt at chasing and repousse in steel. Moving cold steel by hand and hammer is much different than non-ferrous metals. I learned that I needed a bigger hammer with more force and frequent annealing to keep the metal soft enough to work. It gave me an appreciation for medieval armor smiths. I originally designed this to become a door knocker and I eventually will complete it."
-    },
-
-    
-
-    {
-        img: shovel1,
-        medium: 'mild steel',
-        title: 'Heart Shovel',
-        additional_images: [
-            {   
-                title: 'Heart Shovel 1',
-                img: shovel1
-            },
-            {   
-                title: 'Heart Shovel 2',
-                img: shovel2
-            },
-            {   
-                title: 'Heart Shovel 3',
-                img: shovel3
-            }
-        ],
-        description: "I was inspired by some colonial French designs that incorporated a welded heart and wanted to try to make one on a shovel. At the time this was a difficult exercise for me and pushed my forge welding skills to the next level."
-    },
-
-    {
         img: twocandlechandelier1,
         medium: 'mild steel',
         title: 'Two Candle Chandelier',
@@ -455,6 +378,85 @@ const projects = [
             },
         ],
         description: "Tom Latané made a very similar two candle chandelier. I thought the design was fantastic and featured numerous types of forging that I had never done before. The scrolls were especially hard for me at the time. I learned the importance of having quality scrolling tongs that don't bend when twisting."
+    },
+    {
+        img: irongreenman1,
+        medium: 'Mild Steel',
+        title: 'Iron Green Man',
+        additional_images: [
+            {   
+                title: 'Iron Green Man 1',
+                img: irongreenman1
+            },
+            {   
+                title: 'Iron Green Man 2',
+                img: irongreenman2
+            },
+            {   
+                title: 'Iron Green Man 4',
+                img: irongreenman4
+            },
+            {   
+                title: 'Iron Green Man 5',
+                img: irongreenman5
+            },
+        ],
+        description: "This was my first attempt at chasing and repousse in steel. Moving cold steel by hand and hammer is much different than non-ferrous metals. I learned that I needed a bigger hammer with more force and frequent annealing to keep the metal soft enough to work. It gave me an appreciation for medieval armor smiths. I originally designed this to become a door knocker and I eventually will complete it."
+    },
+    {
+        img: leafhook1,
+        medium: 'Mild Steel',
+        title: 'Leaf Hook',
+        additional_images: [
+            {   
+                title: 'Leaf Hook 1',
+                img: leafhook1
+            },
+            {   
+                title: 'Leaf Hook 2',
+                img: leafhook2
+            },
+        ],
+        description: "This hook was inspired by early medieval Gothic-era ironwork, specifically the hinges of the Notre Dame Cathedral. I wanted to develop the process of creating these botanical motifs, using some of the same ancient techniques, to use on more complex future work. There is an elegance and mastery to these shapes that I am only scratching the surface of."
+    },
+    
+
+    {
+        img: shovel1,
+        medium: 'mild steel',
+        title: 'Heart Shovel',
+        additional_images: [
+            {   
+                title: 'Heart Shovel 1',
+                img: shovel1
+            },
+            {   
+                title: 'Heart Shovel 2',
+                img: shovel2
+            },
+            {   
+                title: 'Heart Shovel 3',
+                img: shovel3
+            }
+        ],
+        description: "I was inspired by some colonial French designs that incorporated a welded heart and wanted to try to make one on a shovel. At the time this was a difficult exercise for me and pushed my forge welding skills to the next level."
+    },
+
+    {
+        img: wallsconce1,
+        medium: 'Mild Steel',
+        title: 'Wall Sconce',
+        additional_images: [
+            {   
+                title: 'Wall Sconce 1',
+                img: wallsconce1
+            },
+            {   
+                title: 'Wall Sconce 2',
+                img: wallsconce2
+            },
+        ],
+        description: "We are fond of using candles for light in our household. Taking the candles off surfaces where children and animals can reach them makes it much safer. Since wall mounted candles have been used for centuries I don't think we are the first to have seen the benefit. The inspiration for this design came from a similar one made by mentor and fellow smith Tom Latané. I loved the extreme material isolation and the integrated candle cup. "
     },
 ]
 
