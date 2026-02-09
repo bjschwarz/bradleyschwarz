0b4f1fafd93574f1c125c91f8fce2d5af8361e05
Bradley Schwarz <bjschwa2@gmail.com>
2026-02-09 10:35:35 -0500

    pixel

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,6 +9,7 @@ import Box from '@mui/material/Box';
 import { Padding } from '@mui/icons-material';
 import { GlobalStyles } from '@mui/material';
 import Copyright from '@/components/Copyright';
+import Script from 'next/script';
 
 // app/layout.jsx or app/layout.tsx
 import { GoogleTagManager } from '@next/third-parties/google';
@@ -19,6 +20,31 @@ export default function RootLayout(props) {
   return (
     <html lang="en">
       <body>
+          <Script
+        id="facebook-pixel"
+        strategy="afterInteractive"
+      >
+        {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '2136021650529957');
+          fbq('track', 'PageView');
+        `}
+      </Script>
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: "none" }}
+          src={`https://www.facebook.com/tr?id=2136021650529957&ev=PageView&noscript=1`}
+        />
+      </noscript>
         <AppRouterCacheProvider options={{ enableCssLayer: true }}>
           <ThemeProvider theme={theme}>
             <GoogleTagManager gtmId="G-JH6VP5ZCPR" />
